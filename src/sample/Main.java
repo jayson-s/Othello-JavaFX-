42e26bb0a324a65d3d1f25ebd61a5d2dfef11bd1
Jayson <55456822+jayson-s@users.noreply.github.com>
2020-03-26 03:59:20 -0400

    COMPLETE
    Added all buttons for editing table and file IO
    Added all menubar items with their respective functions
    Sidetab COMPLETE
    Game UI COMPLETE

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,13 +5,9 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -23,7 +19,6 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.*;
 import java.util.logging.Level;
@@ -47,7 +42,6 @@ public class Main extends Application {
 
         //Create menu bar
         final Menu menu1 = new Menu("File");
-        final Menu menu2 = new Menu("Rules");
         MenuBar menuBar = new MenuBar();
 
         //open function to load CSV file with previous games
@@ -201,9 +195,19 @@ public class Main extends Application {
             }
         });
 
+        final Button loadButton = new Button("Load");
+        loadButton.setOnAction(e -> {
+            try {
+                leaderBoard.getItems().clear();
+                readCSV();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
         HBox hb = new HBox();
-        hb.getChildren().addAll(addFirstName, addScore, addButton, saveButton);
-        hb.setSpacing(10);
+        hb.getChildren().addAll(addFirstName, addScore, addButton, saveButton, loadButton);
+        hb.setSpacing(5);
 
         leaderBoard.setItems(data);
         leaderBoard.setEditable(true);
